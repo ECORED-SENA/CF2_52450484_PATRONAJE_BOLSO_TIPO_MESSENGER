4f27247962ae5071a1751130cafdbce5016e60db
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-09-17 08:05:29 -0500

    Cf ajustado - cargue video

--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -12,8 +12,8 @@
 
     figure(data-aos="zoom-in")
       .video.mb-1
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption(data-aos="zoom-in") <strong>Video</strong>: TEXTO
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/DFKajhKy_Jo" title="Elaboración maqueta de comprobación" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption(data-aos="zoom-in") <strong>Video</strong>: Elaboración maqueta de comprobación
 
 </template>
 
